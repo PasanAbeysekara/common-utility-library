069dc2bd96294cd9459e5e47e4aff03dcf5a0176
Tharinda Wickramaarachchi <tharinda.tpw@gmail.com>
2020-07-06 21:56:10 +0530

    Commit missing classes

---
+++ src/main/java/com/solution/x/util/URLProvider.java
@@ -0,0 +1,12 @@
+package com.solution.x.util;
+
+/**
+ * @author Tharinda Wickramaarachchi
+ * @since 7/3/2020 1:13 AM
+ */
+public class URLProvider
+{
+	public final static String SERVICE_DATA = "/data";
+	public final static String SERVICE_SEARCH = "/search";
+	public final static String SERVICE_OPERATION = "/operation";
+}
